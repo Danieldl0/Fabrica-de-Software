eca2d35156b753d397ad1ba868b4c1900ce6650b
Daniel de Oliveira <95375088+Danieldl0@users.noreply.github.com>
2022-02-24 18:45:00 -0300

    add campos: nome e placa no serializer estacionamento

--- api-estacionamento-django/estacionamento/api/serializers.py
+++ api-estacionamento-django/estacionamento/api/serializers.py
@@ -8,7 +8,11 @@ class VeiculoSerializer(serializers.ModelSerializer):
 
 
 class EstacionamentoSerializer(serializers.ModelSerializer):
+    nome_cliente = serializers.CharField(source = "veiculo.nome_cliente", read_only=True)
+    cliente_placa = serializers.CharField(source = "veiculo.placa", read_only=True)
     class Meta:
         model = Estacionamento
-        fields = ("__all__")
+        fields = ("id","veiculo","data_entrada","data_saida","nome_cliente", "cliente_placa")
+    
+    
 
